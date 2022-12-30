2b9c8fc211a909a485f79ba4574df5e4ac0f9823
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2022-12-30 16:11:48 +0100

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-./odoo/src/odoo-bin -c doremistpaul.conf --save --no-database-list --without-demo=all -d doremistpaul --stop-after-init --addons-path=odoo/external-src/account-financial-reporting,odoo/external-src/account-financial-tools,odoo/external-src/account-reconcile,odoo/external-src/event,odoo/external-src/hr,odoo/external-src/vertical-association,odoo/external-src/web,odoo/src/addons,odoo/src/odoo/addons #-i l10n_fr,sale,event,survey,website_sale,crm,sale_management
+
+source venv/bin/activate
+./odoo/src/odoo-bin -c doremistpaul.conf --save --no-database-list --without-demo=all -d doremistpaul --stop-after-init --addons-path=odoo/external-src/account-financial-reporting,odoo/external-src/account-financial-tools,odoo/external-src/account-reconcile,odoo/external-src/event,odoo/external-src/hr,odoo/external-src/vertical-association,odoo/external-src/web,odoo/external-src/l10n-france,odoo/external-src/reporting-engine,odoo/src/addons,odoo/src/odoo/addons #-i l10n_fr,sale,event,survey,website_sale,crm,sale_management

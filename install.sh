9acef3ec1ceb0f7b94c8c51b2c805154db17df19
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2022-12-30 16:29:08 +0100

    add OCA submodules

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source venv/bin/activate
-./odoo/src/odoo-bin -c doremistpaul.conf --save --no-database-list --without-demo=all -d doremistpaul --stop-after-init --addons-path=odoo/external-src/account-financial-reporting,odoo/external-src/account-financial-tools,odoo/external-src/account-reconcile,odoo/external-src/event,odoo/external-src/hr,odoo/external-src/vertical-association,odoo/external-src/web,odoo/external-src/l10n-france,odoo/external-src/reporting-engine,odoo/src/addons,odoo/src/odoo/addons #-i l10n_fr,sale,event,survey,website_sale,crm,sale_management
+./odoo/src/odoo-bin -c doremistpaul.conf --save --no-database-list --without-demo=all -d doremistpaul --stop-after-init --addons-path=odoo/external-src/account-financial-reporting,odoo/external-src/account-financial-tools,odoo/external-src/account-reconcile,odoo/external-src/event,odoo/external-src/hr,odoo/external-src/vertical-association,odoo/external-src/web,odoo/external-src/l10n-france,odoo/external-src/reporting-engine,odoo/external-src/server-tools,odoo/external-src/server-ux,odoo/src/addons,odoo/src/odoo/addons #-i l10n_fr,sale,event,survey,website_sale,crm,sale_management
